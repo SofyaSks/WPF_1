339f2552bbbe028b19c6046a83509c83d7e802f1
sks <ssurovneva@mail.ru>
2023-04-23 00:57:07 +0300

    Add project files.

---
+++ hello/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window // partial - класс распологается в нескольких файлах (скрытых)
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+            //Button b = (Button)sender; // upcast, downcast
+            //b.Width = 200;
+            //b.Height = 400;
+
+            button.Width += 50; // типы double
+            button.Height += 50;
+        }
+    }
+}
